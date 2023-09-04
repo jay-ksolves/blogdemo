f7a4d657a4eb68b94e3c7a87c5166a6cf38a25e0
jay-ksloves <jay.prakash@ksolves.com>
2023-09-04 16:02:20 +0530

    done with error handling

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :posts
   # get 'home' to: 'pages#home'
   get 'about', to: 'pages#about'
    # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
