49e121e90cc8e257765b5a21135cf17a2c542d29
jay-ksloves <jay.prakash@ksolves.com>
2023-10-04 16:43:12 +0530

    done association test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,22 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
   context 'When a user is created' do
     let(:user) { build :user }
     let(:user1) { build :user, name: user.name, email: user.email }
 
     it 'Should be a valid user with all attributes' do
-      # expect(user.valid?).to eq(true)
       expect(user).to be_valid
-      # expect(user.valid?).to  eq(false) #done if user is valid this test case will fails
     end
 
-    it 'Error if a dublicate user is created' do
+    it 'Error if a duplicate user is created' do
       user.save
-      # debugger
       expect(user1.save).to eq(false)
       expect { user1.save! }.to raise_error(ActiveRecord::RecordInvalid)
+    end
+
+    
+    it 'associations' do
+      expect(User.reflect_on_association(:comments).macro).to eq(:has_many)
+      expect(User.reflect_on_association(:posts).macro).to eq(:has_many)
+      expect(User.reflect_on_association(:likes).macro).to eq(:has_many)
     end
   end
 end
